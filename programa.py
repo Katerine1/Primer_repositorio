fa64cbc29fac001b33413c6662909f549afa5af0
Katerine1 <escapealarealidad@gmail.com>
2015-02-09 09:12:52 -0600

    Programa hasta max number trials

---
+++ programa.py
@@ -0,0 +1,17 @@
+import random
+i=1
+while i<=4:
+	generator=random.randint(1,20)
+	print generator
+	number=input("Insert a number of 1 - 20: ")
+	if generator<number:
+		print "Your number is higher than generated, try again"
+		i+=1
+	elif generator>number:
+		print "Your number is lower than generated, try again"
+		i+=1
+	elif generator==number:
+		print "You Win"
+		break
+if i<=4:
+	print "Game over"
